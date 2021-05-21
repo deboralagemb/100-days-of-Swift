da127f56af7f96be821f13097e741c2568a49d10
deboralagemb <deboralagemb@gmail.com>
2021-05-21 14:41:18 -0300

    add: DetailViewController to display selected image

---
+++ Project1/Project1/DetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Débora Lage on 21/05/21.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+}
