e4619f85fef92fbcc8322ff92cc1f6acfdcc76c3
deboralagemb <deboralagemb@gmail.com>
2021-05-21 15:11:35 -0300

    add: display images is sorted order

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -25,6 +25,7 @@ class ViewController: UITableViewController {
                 pictures.append(item)
             }
         }
+        pictures.sort()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
