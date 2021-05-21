786f132c6675e47b9074fcb0b141f48aceeaf091
deboralagemb <deboralagemb@gmail.com>
2021-05-20 21:58:54 -0300

    add: interface

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
     var pictures = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path)
@@ -21,5 +22,15 @@ class ViewController: UIViewController {
                 pictures.append(item)
             }
         }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
     }
 }
