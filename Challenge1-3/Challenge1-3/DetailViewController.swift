4aae64990e46cea688c6973f8cd9a1b069ded220
deboralagemb <deboralagemb@gmail.com>
2021-05-26 15:13:27 -0300

    add: UIActivityViewController to show selected image

--- Challenge1-3/Challenge1-3/DetailViewController.swift
+++ Challenge1-3/Challenge1-3/DetailViewController.swift
@@ -14,6 +14,7 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.largeTitleDisplayMode = .never
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
         imageView.image = UIImage(named: picture!)
         imageView.layer.backgroundColor = UIColor.lightGray.cgColor
     }
@@ -28,4 +29,15 @@ class DetailViewController: UIViewController {
         navigationController?.hidesBarsOnTap = false
     }
 
+    @objc
+    func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image, "Compartilhe a image: \(picture!)"], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
 }
