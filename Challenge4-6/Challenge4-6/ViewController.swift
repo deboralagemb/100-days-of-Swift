72d8a51433a9c7222926274f61d561d2739e7478
deboralagemb <deboralagemb@gmail.com>
2021-06-02 19:43:00 -0300

    add: tableViewController methods to add items

--- Challenge4-6/Challenge4-6/ViewController.swift
+++ Challenge4-6/Challenge4-6/ViewController.swift
@@ -8,7 +8,39 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    var shoppingList = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add item", style: .plain, target: self, action: #selector(addItemToList))
+    }
+    
+    @objc
+    func addItemToList() {
+        let ac = UIAlertController(title: "Add item to list", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        ac.addAction(UIAlertAction(title: "Add", style: .default) { [weak self, weak ac] _ in
+            guard let text = ac?.textFields?[0].text else { return }
+            self?.addItemToTable(item: text)
+        })
+        present(ac, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shoppingList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
+        cell.textLabel?.text = shoppingList[indexPath.row]
+        return cell
+    }
+    
+    func addItemToTable(item: String) {
+        shoppingList.insert(item, at: 0)
+        
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
     }
 }
