95cccb2b4065961bff8f8bcb411193d3e8936def
deboralagemb <deboralagemb@gmail.com>
2021-06-04 22:41:25 -0300

    fix: func loadLevel

--- Project8/Project8/ViewController.swift
+++ Project8/Project8/ViewController.swift
@@ -136,38 +136,38 @@ class ViewController: UIViewController {
     
     func loadLevel() {
         var clueString = ""
-        var solutionsString = ""
+        var solutionString = ""
         var letterBits = [String]()
-        
-        if let levelFileURL = Bundle.main.url(forResource: "level \(level)", withExtension: "txt") {
-            if let levelContents = try? String(contentsOf: levelFileURL) {
+
+        if let levelFilePath = Bundle.main.path(forResource: "level\(level)", ofType: "txt") {
+            if let levelContents = try? String(contentsOfFile: levelFilePath) {
                 var lines = levelContents.components(separatedBy: "\n")
                 lines.shuffle()
-                
+
                 for (index, line) in lines.enumerated() {
                     let parts = line.components(separatedBy: ": ")
                     let answer = parts[0]
                     let clue = parts[1]
-                    
+
                     clueString += "\(index + 1). \(clue)\n"
-                    
+
                     let solutionWord = answer.replacingOccurrences(of: "|", with: "")
-                    solutionsString += "\(solutionWord.count) letters\n"
+                    solutionString += "\(solutionWord.count) letters\n"
                     solutions.append(solutionWord)
-                    
+
                     let bits = answer.components(separatedBy: "|")
                     letterBits += bits
                 }
             }
         }
-        
+
         cluesLabel.text = clueString.trimmingCharacters(in: .whitespacesAndNewlines)
-        answersLabel.text = solutionsString.trimmingCharacters(in: .whitespacesAndNewlines)
-        
-        letterButtons.shuffle()
-        
-        if letterButtons.count == letterBits.count {
-            for i in 0..<letterButtons.count {
+        answersLabel.text = solutionString.trimmingCharacters(in: .whitespacesAndNewlines)
+
+        letterBits.shuffle()
+
+        if letterBits.count == letterButtons.count {
+            for i in 0 ..< letterBits.count {
                 letterButtons[i].setTitle(letterBits[i], for: .normal)
             }
         }
